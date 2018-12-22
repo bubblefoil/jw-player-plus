75207a25058ba846c30d41802cc35737e58fc7b4
Aleš <go.holyal@gmail.com>
2018-12-22 12:24:03 +0100

    Updated to operate with jvs player
    Aktualne has replaced jw-player with jvs-player. This new player can be extended by a plugin to support keyboard shortcuts. However, it ignores events delegated by a userscript. Now, this extension installs the plugin and focuses the video element.

--- jw-player-plus.user.js
+++ jw-player-plus.user.js
@@ -1,13 +1,14 @@
 // ==UserScript==
-// @name         JW-Player shortcuts delegate
+// @name         Video.Aktualne - video player shortcuts
 // @namespace    https://video.aktualne.cz
-// @version      1.1.1
-// @description  Delegate keyboard events to a jw-player element if it is found
-//               in the page so its keyboard shortcuts work without having to click on the player first to focus it.
-//               Also fast-forwards ads.
+// @version      2.0.0
+// @description  Installs videojs plugin to the video player on video.aktualne.cz
+//               Focuses the video element so that keyboard events arrive to the jvs-player.
+//               Also adds playback speed controls.
 // @author       bubblefoil
 // @license      MIT
 // @match        https://video.aktualne.cz/*
+// @require      https://cdn.sc.gl/videojs-hotkeys/latest/videojs.hotkeys.min.js
 // @grant        none
 // @run-at       document-idle
 // ==/UserScript==
@@ -15,18 +16,37 @@
 (function() {
     'use strict';
 
+    let initialized = false;
+
     function getVideo(){
-        return document.querySelector('video.jw-video');
+        return document.querySelector('video.vjs-tech');
     }
 
-    var playbackRates = [0.25, 0.5, 1, 1.125, 1.25, 1.5, 2];
+    function init(){
+        if (initialized) return;
+
+        console.info('Extension: initializing shortcuts.');
+        videojs(getVideo().id).ready(function() {
+            this.hotkeys({
+                volumeStep: 0.1,
+                seekStep: 5,
+                enableModifiersForNumbers: false,
+                alwaysCaptureHotkeys: true
+            });
+        });
+        getVideo().focus();
+        console.info('Extension: shortcuts initialized.');
+        initialized = true;
+    }
+
+    var playbackRates = [0.25, 0.5, 0.75, 1, 1.125, 1.25, 1.5, 2];
     const muteVolume = 0.05;
 
     function slowDown(){
         console.log("slowing down");
         var video = getVideo();
         if(video == null || !video.playbackRate){
-            console.log("There is no jw-player");
+            console.log("There is no video player");
             return;
         }
         var currentRate = video.playbackRate;
@@ -46,28 +66,6 @@
         var ri = playbackRates.findIndex(r => r >= currentRate);
         video.playbackRate = playbackRates[Math.min(ri + 1, playbackRates.length - 1)];
         console.log("Playback rate =", video.playbackRate);
-    }
-
-    let originalVolume = 1;
-    let muted = false;
-    function mute(){
-        if (!muted){
-            originalVolume = getVideo().volume;
-        }
-        getVideo().volume = muteVolume;
-        muted = true;
-    }
-
-    function unmute(){
-        let video = getVideo();
-        if (muted && video) video.volume = originalVolume;
-        muted = false;
-    }
-
-    function skipAd(t){
-        eventFire(t, 'mouseover');
-        eventFire(t, 'mousedown');
-        eventFire(t, 'click');
     }
 
     let originalSpeed = 1;
@@ -96,72 +94,21 @@
         if(e.shiftKey && e.keyCode === 190){
             speedUp();
         }
-        var ctrl = document.getElementsByClassName("jwplayer")[0];
-        if(!ctrl){
-            return false;
-        }
         //Prevent page scroll when pressing Space key
         if(e.keyCode === 32){
             e.preventDefault();
         }
-        var userInactive = ctrl.classList.contains("jw-flag-user-inactive");
-        if(userInactive){
-            //This shows the control bar
-            ctrl.classList.remove("jw-flag-user-inactive");
-        }
-        var cb = document.getElementsByClassName("jw-controlbar")[0];
-        //Focus the player. Clicking this element seems to work
-        cb.click();
         var ed = new KeyboardEvent('keydown', {key: e.key, keyCode: e.keyCode, which: e.which, code: e.code});
         //Send the key event to the player
-        ctrl.dispatchEvent(ed);
-        //Hide the control bar if it was hidden before
-        if(userInactive){
-            ctrl.classList.add("jw-flag-user-inactive");
-        }
+        getVideo().dispatchEvent(ed);
     };
-
-    function eventFire(el, etype){
-        //debugger;
-        if (el.fireEvent) {
-            el.fireEvent('on' + etype);
-        } else {
-            var evObj = document.createEvent('Events');
-            evObj.initEvent(etype, true, false);
-            el.dispatchEvent(evObj);
-        }
-    }
-
-    function getSkippable(){
-        return document.querySelector('.jw-skiptext') || document.querySelector('div.jw-skippable');
-    }
-
-    function addMnemonic(element, key) {
-        if (element && key && key.length === 1 && element.innerText.indexOf(key) > 0) {
-            element.innerHTML = element.innerText.replace(key, `<u>${key}</u>`);
-            element.accessKey = key.toLowerCase;
-        }
-    }
 
     const hasAddedNodes = (mutation) => mutation.addedNodes.length > 0;
 
     const mutationObserver = new MutationObserver(function (mutations) {
-        mutations
-            .filter(hasAddedNodes)
-            .forEach((mutation) => {
-            let s = getSkippable();
-            if (s) {
-                console.debug("click text");
-                mute();
-                fastForward();
-                addMnemonic(s, 'p');
-                skipAd(s);
-            } else {
-                stopFastForward();
-                unmute();
-            }
-            console.trace('Irrelevant mutation.');
-        });
+        if (mutations.some(hasAddedNodes)) {
+            init();
+        }
     });
 
     const observeOptions = {
